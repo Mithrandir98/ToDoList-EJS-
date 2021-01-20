13c02bb7511848aca5736e23ebf657ed684b702f
Ruthwik H Param <aspire1998@gmail.com>
2021-01-20 09:15:16 +0700

    Final commit

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
 
 const app = express();
 
-let items = ["Buy food", "Cook food", "Eat food"];
+let items = [];
 let workItems = [];
 
 app.set("view engine", "ejs"); 
@@ -13,15 +14,8 @@ app.use(express.static("public"));
 
 app.get("/", (req, res) => {
 
-    let today = new Date();
-
-    let options = {
-        weekday: "long",
-        day: "numeric",
-        month: "long"
-    };
-
-    let day = today.toLocaleDateString("en-US", options);
+    //let day = date.getDay();
+    let day = date.getDate();
 
     res.render("list", {listTitle: day, newlistItems: items});
 });
---
+++ date.js
@@ -0,0 +1,28 @@
+
+module.exports.getDate = getDate;
+
+function getDate() {
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    let day = today.toLocaleDateString("en-US", options);
+
+    return day;
+}
+
+//Refactored above output
+exports.getDay = function () {
+
+    let today = new Date();
+
+    let options = {
+        weekday: "long",
+    };
+
+    return today.toLocaleDateString("en-US", options);
+};
